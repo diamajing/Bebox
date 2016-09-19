5fa99e07d1d80480811e6db3ef10fd7a09f3e5b3
amine <amine@videri-concept.de>
2016-09-19 10:13:27 +0200

    Backend optimisation
    Bebox edit : text_field to collection_select
    content_for tag

--- Bebox-system/app/helpers/backend_helper.rb
+++ Bebox-system/app/helpers/backend_helper.rb
@@ -1,4 +1,20 @@
 module BackendHelper
+
+  def bc_link(name, link = nil)
+    content_tag :li, itemscope: 'itemscope', itemtype: 'http://data-vocabulary.org/Breadcrumb', class: (link ? nil : 'active') do
+      if link
+        link_to link, itemprop: 'url' do
+          content_tag :span, itemprop: 'title' do
+            name
+          end
+        end
+      else
+        content_tag :span, itemprop: 'title' do
+          name
+        end
+      end
+    end
+  end
 	
   def menu_active(c_name, second = true)
     return 'active' if controller_name == c_name && second
