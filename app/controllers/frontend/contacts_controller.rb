0b3b0a7a67b97c4781c1ac40d5de1fe326035b78
Amine Belkhiria <amine@videri-concept.de>
2017-03-02 15:45:09 +0100

    template shop

--- app/controllers/frontend/contacts_controller.rb
+++ app/controllers/frontend/contacts_controller.rb
@@ -8,6 +8,8 @@ class Frontend::ContactsController < ApplicationController
       redirect_to root_path , notice: "Une Erreur s'est produite, le message n'a pas ete envoye. Veuillez essayer encore"
         end 
     end
+    
+    private
     def contact_params
   		params.require(:contact).permit(:name,:surname, :company , :email , :Telephone,:message)
   	end
