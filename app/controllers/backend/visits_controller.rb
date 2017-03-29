705515ed98e69700aa80341fac7fa19c1fbdb3d9
Amine Belkhiria <amine@videri-concept.de>
2017-03-29 23:26:02 +0200

    visits new

--- app/controllers/backend/visits_controller.rb
+++ app/controllers/backend/visits_controller.rb
@@ -2,4 +2,23 @@ class Backend::VisitsController < BackendController
 	def index
 		@visits = Visit.order(created_at: :desc)
 	end
+
+	def new
+		@visit = Visit.new
+	end
+
+	def create
+		visit = Visit.new(visit_params)
+		if visit.save
+			redirect_to backend_visits_path, notice: 'Visite client a ete ajoutee'
+		else
+			render 'new', alert: "Une erreur s'est produite. Veuillez reessayer"
+		end
+	end
+
+	private
+
+	def visit_params
+  		params.require(:visit).permit(:customer_name, :telephone , :location , :report)
+  	end
 end
